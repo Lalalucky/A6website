d7a94070cdfcf97ac82b310c4d81494d3eb0dcbb
yunbao1 <782871476@qq.com>
2019-03-21 18:13:34 +0800

    0321提交

--- website/src/views/Home/index.vue
+++ website/src/views/Home/index.vue
@@ -1,17 +1,19 @@
 <template>
 <!-- 官网主页 -->
    <Top></Top>
+   <Banner></Banner>
 </template>
 
 <script>
-import Top from '@/components/top.vue'
+import Top from '@/components/top.vue';
+import Banner from '@/components/banner.vue'
 export default {
    name: '',
    data() {
        return {}
    },
   components: {
-      Top
+      Top,Banner
   }
 }
 </script>
