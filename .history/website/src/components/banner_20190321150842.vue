e6a7542f85d31a43e0dcd8e58fdcb7238e1896c7
yunbao1 <782871476@qq.com>
2019-03-21 18:15:29 +0800

    没网了

---
+++ .history/website/src/components/banner_20190321150842.vue
@@ -0,0 +1,20 @@
+<template>
+    <el-carousel indicator-position="outside">
+        <el-carousel-item v-for="item in 4" :key="item">
+            <h3>{{ item }}</h3>
+        </el-carousel-item>
+    </el-carousel>
+</template>
+
+<script>
+export default {
+   name: '',
+   data() {
+       return {}
+   },
+  components: {}
+}
+</script>
+
+<style scoped lang="less">
+</style>
---
+++ .history/website/src/views/Home/index_20190321181012.vue
@@ -0,0 +1,22 @@
+<template>
+<!-- 官网主页 -->
+   <Top></Top>
+   <Banner></Banner>
+</template>
+
+<script>
+import Top from '@/components/top.vue';
+import Banner from '@/components/banner.vue'
+export default {
+   name: '',
+   data() {
+       return {}
+   },
+  components: {
+      Top,Banner
+  }
+}
+</script>
+
+<style scoped lang="less">
+</style>
